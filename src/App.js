94f7fda837d4b9f7c2c8a6f72e080cb3968e7d7c
Linssi Colin Ruiz <linssi101@gmail.com>
2023-01-24 21:10:40 -0600

    lecture into redux set up reducers, redux actions, redux state logger, redux react spread.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 // useSelector is a hook provided by the react-redux library
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
+import ElementList from "./ElementList";
 
-function App () {
-    // useSelector accepts a function that tells it what part of the store you want.
-    // here we return the whole store
-    const reduxStore = useSelector(store => store)
-    const count = useSelector(store => store.count)
+function App() {
+	// useSelector accepts a function that tells it what part of the store you want.
+	// here we return the whole store
+	const reduxStore = useSelector((store) => store);
+	const count = useSelector((store) => store.count);
+	const elementList = useSelector((store) => store.elementList);
 
+	// dispatch instance
+	const dispatch = useDispatch();
 
-    return (
-      <div>
-        {/* Render the entire reduxStore to our DOM, as a JS object (JSON) */}
-        <pre>{JSON.stringify(reduxStore)}</pre>
-        <p>Count is: {count}</p>
+	// hooks
+	const [newElement, setNewElement] = useState("");
 
-      </div>
-    );
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		dispatch({
+			type: "ADD_ELEMENT",
+			payload: newElement,
+		});
+		setNewElement("");
+	};
 
+	return (
+		<div>
+			{/* Render the entire reduxStore to our DOM, as a JS object (JSON) */}
+			<pre>{JSON.stringify(reduxStore)}</pre>
+			<p>Count is: {count}</p>
+			<button onClick={() => dispatch({ type: "INCREASE" })}>
+				Increase Count
+			</button>
+			<button onClick={() => dispatch({ type: "DECREASE" })}>
+				Decrease Count
+			</button>
+			{/* <p>Elements are: {JSON.stringify(elementList)}</p> */}
+			<ElementList />
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Element name goes here"
+					value={newElement}
+					onChange={(event) => setNewElement(event.target.value)}
+				/>
+				<button type="submit">Add Element</button>
+			</form>
+
+			{/* <p>{elementList[0]}</p> */}
+			{/* <button */}
+			{/* // onClick={() => dispatch({ type: "ADD_ELEMENT", payload: "radium" })}
+			// >
+			// 	Add Element
+			// </button> */}
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 
 // reducer!
 const count = (state = 0, action) => {
-    console.log(`Hey!!! I'm a reducer y'all!!!`);
-
-    return state;
+	console.log(`Hey!!! I'm a reducer y'all!!!`);
+	console.log(action);
+	if (action.type === "INCREASE") {
+		state = state + 1;
+	} else if (action.type === "DECREASE") {
+		state = state - 1;
+	}
+	return state;
 };
 
-const elementList = (state = [], action) => {
-  return state;
+const elementList = (state = ["Jupiter", "Saturn"], action) => {
+	console.log("inside element list", action.payload);
+	if (action.type === "ADD_ELEMENT") {
+		return [...state, action.payload];
+	}
+	return state;
 };
 
 // store!
 const storeInstance = createStore(
-  combineReducers(
-      {
-          count,
-          elementList,
-      }
-  )
+	combineReducers({
+		count,
+		elementList,
+	}),
+	// console tool (help debug and code)
+	applyMiddleware(logger)
 );
 
 // Provider lets redux and react talk to one another
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={storeInstance}>
-        <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={storeInstance}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
